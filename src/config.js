2bc039c87822b2e55bf16a29c5bb329be3a69014
Bipin Giri <bipingiri@Bipins-MacBook-Air.local>
2026-06-18 15:48:03 +0545

    upgrade vite to use env

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
   oauth: {
-    clientId: '1f966b5f-9b47-413c-b7a9-9eb2c30dd772',
-    clientSecret: 'UT1CUaS2EbULnkVe2NWK1Ptf0QNKd4LcvDWg8Fcc',
-    redirectUri: 'http://localhost:8000/redirect',
-    authUrl: 'https://rca.oneflow.test/oauth/authorize',
-    tokenUrl: 'https://rca.oneflow.test/oauth/token',
-    scope: 'accounts.read branches.read reporting_tags.read reports.balance_sheet.read reports.income_statement.read reports.trial_balance.read offline_access',
+    clientId: import.meta.env.VITE_CLIENT_ID,
+    clientSecret: import.meta.env.VITE_CLIENT_SECRET,
+    redirectUri: import.meta.env.VITE_REDIRECT_URI,
+    authUrl: import.meta.env.VITE_AUTH_URL,
+    tokenUrl: import.meta.env.VITE_TOKEN_URL,
+    scope: import.meta.env.VITE_SCOPE,
   },
   api: {
-    baseUrl: 'https://rca.oneflow.test/api/v2',
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
   },
 };
 
